83cc58611e85746115e7c30c576f280c8f46eafb
Pedro Pablo Osorio San Martín <pedro.osorio04@inacapmail.cl>
2025-06-06 17:46:16 -0400

    Update urls.py

--- backend/apps/category/urls.py
+++ backend/apps/category/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CategoryListView, CreateCategoryAPIView
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view(), name='category_list'), # List all categories
+    path('/', CategoryListView.as_view(), name='category_list'), # List all categories
     path('create/', CreateCategoryAPIView.as_view(), name='create-customer'),
 
 ]
