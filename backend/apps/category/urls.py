b4387a14a54f54f0bd131b7b8331be58a3dc379b
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-06-06 14:51:18 -0400

    se crea el modelo, serializers, endpoint y urls de la categoria

---
+++ backend/apps/category/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import CategoryListView, CreateCategoryAPIView
+
+urlpatterns = [
+    path('categories/', CategoryListView.as_view(), name='category_list'), # List all categories
+        path('create/', CreateCategoryAPIView.as_view(), name='create-customer'),
+
+]
--- backend/apps/category/views.py
+++ backend/apps/category/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+
+class CategoryListAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        customers = Category.objects.all()
+        serializer = CategorySerializer(customers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class CreateCategoryAPIView(CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+    
