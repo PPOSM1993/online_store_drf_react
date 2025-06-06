54da5476ac0eae992f6c3af1759ae35b7bdec559
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-06-06 12:35:41 -0400

    se crea endpoint de actualizar info cliente

--- backend/apps/customers/urls.py
+++ backend/apps/customers/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', CustomersListAPIView.as_view(), name='customers-list'),  # 👈 ahora es raíz
     path('create/', CreateCustomerAPIView.as_view(), name='create-customer'),
+    path('edit/<int:pk>/', CustomerDetail, name='customer-detail-or-update'),
     path('delete/<int:pk>/', DeleteCustomer, name='delete-customer'),
     path('search/', SearchCustomers, name='search-customers'),
 
