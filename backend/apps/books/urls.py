46b600e74c7bb87c9e2f689170872e863dd9a615
Pedro Pablo Osorio San Martín <pedro.osorio04@inacapmail.cl>
2025-06-12 16:45:00 -0400

    Update urls.py

--- backend/apps/books/urls.py
+++ backend/apps/books/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, AuthorListAPIView, PublisherAPIView
+from .views import BookViewSet, AuthorListAPIView, PublisherAPIView, SearchBooks
 from django.urls import path
 
 
