c766e87844996cc1b7797d82a4022b711b96930b
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-06-17 16:19:03 -0400

    se finaliza y se prueba el funcionamiento del crud en proveedores

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 
-import { Login, Dashboard, Register, CustomerList, CustomerForm, CategoryForm, CategoryList, BooksList, BooksForm, SuppliersFrom } from './index'
+import { Login, Dashboard, Register, CustomerList, CustomerForm, CategoryForm, CategoryList, BooksList, BooksForm, SuppliersFrom, SuppliersList } from './index'
 
 
 function App() {
@@ -40,8 +40,10 @@ function App() {
         <Route path="/books/create" element={isAuthenticated() ? <BooksForm /> : <Navigate to="/login" />} />
         <Route path="/books/edit/:id" element={isAuthenticated() ? <BooksForm /> : <Navigate to="/login" />} />
 
-        <Route path="/suppliers/create" element={isAuthenticated() ? <SuppliersFrom /> : <Navigate to="/login" />} />
 
+        <Route path="/suppliers" element={isAuthenticated() ? <SuppliersList /> : <Navigate to="/login" />} />
+        <Route path="/suppliers/create" element={isAuthenticated() ? <SuppliersFrom /> : <Navigate to="/login" />} />
+        <Route path="/suppliers/edit/:id" element={isAuthenticated() ? <SuppliersFrom /> : <Navigate to="/login" />} />
       </Routes>
     </Router>
   )
