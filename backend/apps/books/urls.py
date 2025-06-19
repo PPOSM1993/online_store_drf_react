140c33458f7278c9015355d016ac355488ac3611
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-06-19 17:44:17 -0400

    se arregla un condoro en la url de books

--- backend/apps/books/urls.py
+++ backend/apps/books/urls.py
@@ -1,14 +1,18 @@
 from rest_framework.routers import DefaultRouter
-from .views import BookViewSet, AuthorListAPIView, PublisherAPIView
+from .views import CreateBookAPIView, AuthorListAPIView, PublisherAPIView, SearchBooks, DeleteBook, BookDetail
+from .views import AuthorListAPIView, PublisherAPIView
 from django.urls import path
 
 
 router = DefaultRouter()
-router.register(r'', BookViewSet, basename='books')
+#router.register(r'create', BookViewSet, basename='books')
 urlpatterns = router.urls
 
 urlpatterns = [
+    path('create/', CreateBookAPIView.as_view(), name='create_book'),
     path('author/', AuthorListAPIView.as_view(), name='author_list'),
     path('publisher/', PublisherAPIView.as_view(), name='publisher_list'),
     path('search/', SearchBooks, name='search-books'),
+    path('delete/<int:pk>/', DeleteBook, name='delete-book'),
+    path('edit/<int:pk>/', BookDetail, name='book-detail-or-update'),
 ]
